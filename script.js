7c12bd3e6ed5cd618eff4b63c14e4669865c002b
thennuthennu56@gmail.com <cdesappan333@gmail.com>
2025-09-28 19:47:55 +0530

    Final Update

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ window.addEventListener("scroll",function(){
     header.classList.toggle("sticky", window.scrollY > 0);
 
 });
+
+let menu = document.querySelector('#menu-icons');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-xmark');
+    menu.classList.toggle('fa-bars');
+    navbar.classList.toggle('open');
+
+};
+
+
